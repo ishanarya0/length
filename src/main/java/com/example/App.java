1af05a356089fe4f4a2010b78ee94c63bb75af40
GrayFlash <gk74533@gmail.com>
2021-06-15 11:21:35 +0530

    Modify testing samples for differentvalues

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -4,11 +4,11 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         CheckLength obj = new CheckLength();
-        String x1 = "800";
-        String y1 = "900";
-        String x2 = "800";
-        String y2 = "1000";
-        int precision = 8;
+        String x1 = "0";
+        String y1 = "9";
+        String x2 = "0";
+        String y2 = "12";
+        int precision = 2;
         System.out.println(obj.findLength(x1, y1, x2, y2, precision));
     }
 }
